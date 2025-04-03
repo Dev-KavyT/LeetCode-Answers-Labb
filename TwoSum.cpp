bea72f1d5cb946c4c732c931af46f42416c92afa
Dev-KavyT <thkrjanak@gmail.com>
2025-04-03 16:24:31 +0530

    Update TwoSum.cpp

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -1,24 +1,19 @@
-#include <iostream>
-#include <vector>
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numMap; 
+        
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
 
-using namespace std;
-
-vector<int> twoSum(vector<int>& nums, int target) {
-    int n = nums.size();
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (nums[i] + nums[j] == target) {
-                return {i, j};
+            if (numMap.find(complement) != numMap.end()) {
+                return {numMap[complement], i};
             }
-        }
-    }
-    return {}; 
-}
 
-int main() {
-    vector<int> nums = {2, 7, 11, 15};
-    int target = 9;
     
-    vector<int> result = twoSum(nums, target);
-    cout << result[0] << " " << result[1] << endl; 
-}
+            numMap[nums[i]] = i;
+        }
+        return {}; 
+    }
+};
+
