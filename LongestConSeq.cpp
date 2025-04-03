87fa4a437b98f7d209633a355d8b824740b837c6
Dev-KavyT <thkrjanak@gmail.com>
2025-04-03 16:25:20 +0530

    Update LongestConSeq.cpp

--- LongestConSeq.cpp
+++ LongestConSeq.cpp
@@ -1,11 +1,8 @@
-#include <iostream>
-#include <vector>
-#include <unordered_set>
 
-using namespace std;
-
-int longestConsecutive(vector<int>& nums) {
-    unordered_set<int> numSet(nums.begin(), nums.end()); 
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+           unordered_set<int> numSet(nums.begin(), nums.end()); 
     int longestStreak = 0;
 
     for (int num : numSet) {
@@ -22,10 +19,5 @@ int longestConsecutive(vector<int>& nums) {
         }
     }
     return longestStreak;
-}
-
-int main() {
-    vector<int> nums = {100, 4, 200, 1, 3, 2};
-    cout  << longestConsecutive(nums) << endl;
-    return 0;
-}
+    }
+};
